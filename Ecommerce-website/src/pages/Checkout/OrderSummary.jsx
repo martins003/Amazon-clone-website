e2e669504c1be6b4a032440c3504fd7d437ca704
Francis Umeokoli <ciscoumeh@gmail.com>
2025-08-25 12:41:02 +0100

    create delete cart item feature

--- Ecommerce-website/src/pages/Checkout/OrderSummary.jsx
+++ Ecommerce-website/src/pages/Checkout/OrderSummary.jsx
@@ -1,8 +1,9 @@
 import dayjs from 'dayjs';
+import axios from 'axios';
 import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
 
-export function OrderSummary({ cart, deliveryOptions , loadCart }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
@@ -11,6 +12,10 @@ export function OrderSummary({ cart, deliveryOptions , loadCart }) {
             return deliveryOption.id === cartItem.deliveryOptionId
           });
 
+        const deleteCartItem = async () => {
+          await axios.delete(`/api/cart-items/${cartItem.productId}`);
+          await loadCart();
+        }
         return (
           <div key={cartItem.productId} className="cart-item-container">
             <div className="delivery-date">
@@ -35,13 +40,13 @@ export function OrderSummary({ cart, deliveryOptions , loadCart }) {
                   <span className="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span className="delete-quantity-link link-primary">
+                  <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
                     Delete
                   </span>
                 </div>
               </div>
 
-              <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart}/>
+              <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart} />
             </div>
           </div>
         )
