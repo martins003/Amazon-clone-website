90f7bd2f73c5a5d0a801336fe3b737e56866d6a5
Francis Umeokoli <ciscoumeh@gmail.com>
2025-08-27 13:07:38 +0100

    use beforeEach to remove duplication

--- Ecommerce-website/src/pages/home/product.test.jsx
+++ Ecommerce-website/src/pages/home/product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -7,8 +7,13 @@ import { Product } from './Product';
 vi.mock('axios');
 
 describe('Product component', () => {
-  it('displays the product details correctly', () => {
-    const product = {
+
+  let product;
+
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,8 +24,12 @@ describe('Product component', () => {
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"]
     };
+    loadCart = vi.fn();
+  })
+  // runs some code before each test so that one test does not affect the other
 
-    const loadCart = vi.fn();
+  it('displays the product details correctly', () => {
+
 
     render(<Product product={product} loadCart={loadCart} />)
 
@@ -47,19 +56,7 @@ describe('Product component', () => {
   });
 
   it('adds a product to the cart', async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"]
-    };
 
-    const loadCart = vi.fn();
 
     render(<Product product={product} loadCart={loadCart} />);
 
@@ -72,7 +69,7 @@ describe('Product component', () => {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 1
     }
-  );
-  expect(loadCart).toHaveBeenCalled();
+    );
+    expect(loadCart).toHaveBeenCalled();
   })
 });
